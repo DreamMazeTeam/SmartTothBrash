3b4ba10f65cb9a3c82990aaa6efae1a644cffab2
DreamMazeTeam <58484975+DreamMazeTeam@users.noreply.github.com>
2020-02-27 22:09:31 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,32 @@
-#include <Arduino.h>
+// Раскоментировать строку для компиляции в Arduino IDE
+//#define ARDUINO_IDE
 
+
+#ifndef ARDUINO_IDE
+#include <Arduino.h>
+#endif
 
 // Для отладки
 #define DB_SERIAL
 
 // Пины
 #define GL_PIN_UP 0
-#define GL_PIN_DONW 0
+#define GL_PIN_DOWN 0
 #define GR_PIN_UP 0
 #define GR_PIN_DOWN 0
 
-const int GYRO_PINS_COUNT = 4;
+// Чуть чуть редефов
+#define G_PIN_180 GR_PIN_UP
+#define G_PIN_90_UP GL_PIN_UP
+#define G_PIN_90_DOWN GL_PIN_DOWN
+
+
+const int GYRO_PINS_COUNT = 3;
 const int GYRO_PINS[GYRO_PINS_COUNT] = {
-    GL_PIN_UP,
-    GL_PIN_DONW,
-    GR_PIN_UP,
-    GR_PIN_DOWN
+    G_PIN_90_UP,
+    G_PIN_90_DOWN,
+    G_PIN_180,
+//    GR_PIN_DOWN
 };
 
 
