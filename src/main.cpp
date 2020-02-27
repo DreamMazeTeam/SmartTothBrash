15194500f672c9279bd6c7fd7aadcf4723547485
DreamMazeTeam <58484975+DreamMazeTeam@users.noreply.github.com>
2020-02-27 21:31:43 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,29 @@
 #include <Arduino.h>
 
+
 #define DB_SERIAL
+
+
+#ifdef DB_SERIAL
+void db_print()
+{
+
+}
+#endif
+
 
 void setup()
 {
 #ifdef DB_SERIAL
     Serial.begin(9600);
+    Serial.println("Debug mod -> on");
 #endif
 }
 
+
 void loop() 
 {
-
+#ifdef DB_SERIAL
+    db_print();
+#endif
 }
