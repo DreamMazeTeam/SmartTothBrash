3bb4a33c877671ac32a0da47bc38fb29a60fa13d
DreamMazeTeam <58484975+DreamMazeTeam@users.noreply.github.com>
2020-02-27 22:00:11 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,22 @@
 #include <Arduino.h>
 
 
+// Для отладки
 #define DB_SERIAL
+
+// Пины
+#define GL_PIN_UP 0
+#define GL_PIN_DONW 0
+#define GR_PIN_UP 0
+#define GR_PIN_DOWN 0
+
+const int GYRO_PINS_COUNT = 4;
+const int GYRO_PINS[GYRO_PINS_COUNT] = {
+    GL_PIN_UP,
+    GL_PIN_DONW,
+    GR_PIN_UP,
+    GR_PIN_DOWN
+};
 
 
 #ifdef DB_SERIAL
@@ -18,6 +33,11 @@ void setup()
     Serial.begin(9600);
     Serial.println("Debug mod -> on");
 #endif
+
+    // Настройка нашего всевдо гироскопа
+    for (int i = 0; i < GYRO_PINS_COUNT; i++){
+        pinMode(GYRO_PINS[i], INPUT);
+    }
 }
 
 
